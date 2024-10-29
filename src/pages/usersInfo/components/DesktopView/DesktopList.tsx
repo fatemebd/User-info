758962edd2346717d493cd4f29756ca8ee3d794a
fateme <fateme.s.e.81@gmail.com>
2024-10-30 02:39:33 +0330

    feat: add desktop view

---
+++ src/pages/usersInfo/components/DesktopView/DesktopList.tsx
@@ -0,0 +1,73 @@
+import { Modal, Table } from "antd";
+import { usersInfoColumns } from "./constants/UsersInfoColumns";
+import { User } from "../../api/api.types";
+import { useState } from "react";
+import UserInfoCard from "../../../../components/UserInfo/UserInfo";
+import { useGetUsersInfo } from "../../api";
+
+interface DesktopListProps {
+  setCurrentPage: (arg: number) => void;
+  setPageSize: (arg: number) => void;
+  isLoading: boolean;
+  usersInfo: User[];
+  page: number;
+  perPage: number;
+  total: number;
+}
+
+const DesktopList = ({
+  setCurrentPage,
+  setPageSize,
+  isLoading,
+  usersInfo,
+  page,
+  perPage,
+  total,
+}: DesktopListProps) => {
+  const [modalUser, setModalUser] = useState<User>();
+
+  const columns = usersInfoColumns();
+  const handleCloseModal = () => setModalUser(undefined);
+
+  const handleTableChange = (pagination: any) => {
+    setCurrentPage(pagination.current);
+    setPageSize(pagination.pageSize);
+  };
+
+  return (
+    <div>
+      <Modal
+        open={!!modalUser}
+        title={`${modalUser?.first_name}'s details`}
+        onClose={handleCloseModal}
+        onCancel={handleCloseModal}
+        onOk={handleCloseModal}
+      >
+        <UserInfoCard
+          className="border-0"
+          user={modalUser!}
+          isLoading={isLoading}
+        />
+      </Modal>
+      <Table
+        onRow={(record) => ({
+          onClick: () => setModalUser(record),
+          className: "cursor-pointer",
+        })}
+        loading={isLoading}
+        columns={columns}
+        dataSource={usersInfo}
+        onChange={handleTableChange}
+        pagination={{
+          current: page,
+          pageSize: perPage,
+          total: total,
+          showSizeChanger: true,
+          pageSizeOptions: ["5", "10", "15"],
+        }}
+      />
+    </div>
+  );
+};
+
+export default DesktopList;
---
+++ src/pages/usersInfo/components/DesktopView/constants/UsersInfoColumns.tsx
@@ -0,0 +1,29 @@
+import { Avatar, Typography } from "antd";
+import { User } from "../../../api/api.types";
+
+interface UsersInfoColumnsArgs {
+  setModalUser:()=>void;
+}
+
+export const usersInfoColumns = () => {
+  return [
+    {
+      dataIndex: "avatar",
+      key: "avatar",
+      render: (avatarUrl: string) => <Avatar src={avatarUrl} />,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (_: undefined, record: User) => (
+        <Typography>{`${record.first_name} ${record.last_name}`}</Typography>
+      ),
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+  ];
+};
